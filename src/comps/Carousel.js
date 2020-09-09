6a92e626782514656e776da3eed9e85616df782f
jaredLo <gerardlo.psd@gmail.com>
2020-09-10 06:11:15 +0800

    before i do something drastic

---
+++ src/comps/Carousel.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Flickity from "react-flickity-component";
+import "../flickity.css";
+
+export default function Carousel() {
+  return (
+    <>
+      <Flickity>
+        <div>1</div>
+        <div>2</div>
+        <div>3</div>
+      </Flickity>
+    </>
+  );
+}
--- src/comps/Testimonial.js
+++ src/comps/Testimonial.js
@@ -1,15 +1,34 @@
 import React from "react";
-import Carousel from "react-multi-carousel";
 import "react-multi-carousel/lib/styles.css";
 import arrow from "../assets/7-icons/arrow-right.svg";
+import Carousell from "./Carousel";
+import Simple from "./Carousel";
 
 function Testimonial() {
+  const responsive = {
+    desktop: {
+      breakpoint: { max: 3000, min: 1024 },
+      items: 3,
+      slidesToSlide: 3, // optional, default to 1.
+    },
+    tablet: {
+      breakpoint: { max: 1024, min: 464 },
+      items: 2,
+      slidesToSlide: 2, // optional, default to 1.
+    },
+    mobile: {
+      breakpoint: { max: 464, min: 0 },
+      items: 1,
+      slidesToSlide: 1, // optional, default to 1.
+    },
+  };
   return (
     <div className="testimonial">
       <div className="testimonial-top">
         <h1>What other people say about our service</h1>
         <img className="arrow-right" src={arrow} alt="" />
       </div>
+      <Simple />
     </div>
   );
 }
